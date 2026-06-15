66dd4e0ab68a74db219912c1303a5da866d97f2d
Ulrike Fischer <fischer@troubleshooting-tex.de>
2026-06-15 18:41:37 +0200

    update version

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 -- Build script for pdfcomment
-packageversion="2.4a"
-packagedate="2018-11-01"
+packageversion="3.0b"
+packageversionlegacy="2.4b"
+packagedate="2026-06-15"
 
 module   = "pdfcomment"
 ctanpkg  = "pdfcomment"
@@ -40,3 +41,43 @@ specialtypesetting["example_math_markup.tex"] = {cmd = "pdflatex -interaction=no
 function docinit_hook() 
 cp("pdfcomment.sty", unpackdir, typesetdir)
 return cp("pdfcomment-2018-11-01.sty", unpackdir, typesetdir) end
+
+
+tagfiles = {
+            "pdfcomment.dtx",
+            "pdfcomment-2018-11-01.sty",
+            "pdfcomment.tex",
+            "README.md"
+           }
+
+
+function update_tag (file,content,tagname,tagdate) 
+ if string.match (file, "%.dtx$" ) then
+  content = string.gsub (content,
+                         "\\ProvidesExplPackage {(.-)} {.-} {.-}",
+                         "\\ProvidesExplPackage {%1} {" .. packagedate.."} {"..packageversion .. "}")
+  content = string.gsub (content,
+                         "\\date{.-}",
+                         "\\date{v"..packageversion..", released ".. packagedate.."}")
+  return content
+elseif string.match (file, "%.sty$" ) then
+  content = string.gsub (content,
+                         "\\ProvidesPackage{(.-)}%[.+ Josef",
+                         "\\ProvidesPackage{%1}[" .. packagedate .." pdfcomment.sty v"..packageversionlegacy .." - Josef")
+  return content
+ elseif string.match (file, "^README.md$") then
+   content = string.gsub (content,
+                         "Version: %d%.%d+%l?",
+                         "Version: " .. packageversion )
+   return content
+ elseif string.match (file, "%.tex$" ) then
+   content = string.gsub (content,
+                         "\\date{.-}",
+                         "\\date{"..packagedate.."}")
+   content = string.gsub (content,
+                         "\\Fileversion{.-}",
+                         "\\Fileversion{v"..packageversion.."}")
+   return content
+ end
+ return content
+ end
